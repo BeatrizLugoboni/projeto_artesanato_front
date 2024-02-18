623837f4bbce4e60e79d55325962f5c1aa3aecc8
Igor Ribeiro <igor.ribeiro@uotz.com.br>
2024-02-18 12:56:46 -0300

    Cria layout base da aplicação
    - separa o layout em componentes
    - cria a AppBar
    - cria a estrutura de layout para renderizar a estrutura da rota

---
+++ src/components/AppBar.vue
@@ -0,0 +1,28 @@
+<template>
+    <v-app-bar absolute app elevation="2" color="primary">
+        <v-toolbar-title>
+            <span>
+                Artesanato
+            </span>
+        </v-toolbar-title>
+        <v-spacer></v-spacer>
+        <v-btn icon>
+            <v-icon>mdi-basket</v-icon>
+        </v-btn>
+        <v-btn icon>
+            <v-icon>mdi-heart</v-icon>
+        </v-btn>
+        <ProductDialog />
+    </v-app-bar>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    },
+    components: {
+        ProductDialog: () => import('@/components/ProductDialog.vue')
+    }
+};
+</script>
+<style scoped></style>
---
+++ src/components/AppLayout.vue
@@ -0,0 +1,16 @@
+<template>
+    <v-main>
+        <v-container>
+            <router-view></router-view>
+        </v-container>
+    </v-main>
+</template>
+<script>
+export default {
+    data() {
+        return {}
+    }
+};
+
+</script>
+<style scoped></style>
